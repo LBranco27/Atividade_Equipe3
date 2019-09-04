4bd5c3b277690e3b9a4687759cda24896bd5bdc1
Kire87 <erikff7@gmail.com>
2019-09-04 08:41:00 -0300

    Bota componente com meu nome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErikComponent } from './erik/erik.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErikComponent
   ],
   imports: [
     BrowserModule
